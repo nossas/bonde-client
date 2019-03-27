767958ccee5281f9c55322863e5f668ecbb506d5
Igor Santos <igr.exe@gmail.com>
2019-03-27 17:58:15 -0300

    feat(admin-canary): add chatbot workflow form to create workflow

--- packages/bonde-admin-canary/src/scenes/Logged/scenes/Workflows/Workflows.js
+++ packages/bonde-admin-canary/src/scenes/Logged/scenes/Workflows/Workflows.js
@@ -1,31 +1,32 @@
 import React from 'react'
-import { Grid, Cell, Flexbox2 as Flexbox, Title } from 'bonde-styleguide'
-import { Auth } from 'services/auth'
+import {
+  Cell,
+  Flexbox2 as Flexbox,
+  Grid,
+  Title
+} from 'bonde-styleguide'
 import { Page, Header } from 'components/PageLogged'
-import { ChatbotWorkflowList } from './components'
+import { ChatbotWorkflowList, ChatbotWorkflowModalForm } from './components'
 
 const COMMUNITY = { id: 1, name: 'Bonde' }
 
 export default class extends React.Component {
   render () {
     return (
-      <Auth>
-        {({ user }) => (
-          <Page
-            renderTitle={() => (<Header.Title>{COMMUNITY.name}</Header.Title>)}
-          >
-            <Flexbox vertical>
-              <Title.H2 margin={{ bottom: 10 }}>Chatbot</Title.H2>
-              <Title.H5 margin={{ bottom: 25 }}>FLUXOS DE CONVERSA</Title.H5>
-              <Grid>
-                <Cell size={[12, 12, 12]}>
-                  <ChatbotWorkflowList communityId={COMMUNITY.id} />
-                </Cell>
-              </Grid>
-            </Flexbox>
-          </Page>
-        )}
-      </Auth>
+      <Page renderTitle={() => (<Header.Title>{COMMUNITY.name}</Header.Title>)}>
+        <Flexbox vertical>
+          <Flexbox horizontal spacing='between'>
+            <Title.H2 margin={{ bottom: 10 }}>Chatbot</Title.H2>
+            <ChatbotWorkflowModalForm />
+          </Flexbox>
+          <Title.H5 margin={{ bottom: 25 }}>FLUXOS DE CONVERSA</Title.H5>
+          <Grid>
+            <Cell size={[12, 12, 12]}>
+              <ChatbotWorkflowList communityId={COMMUNITY.id} />
+            </Cell>
+          </Grid>
+        </Flexbox>
+      </Page>
     )
   }
 }
