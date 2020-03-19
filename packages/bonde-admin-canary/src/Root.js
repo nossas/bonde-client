584744405486ee04a861cd944376b5fa0dbd60e7
Igor Santos <igr.exe@gmail.com>
2020-03-19 13:49:27 -0300

    feat(canary): add bonde-core-tools

--- packages/bonde-admin-canary/src/Root.js
+++ packages/bonde-admin-canary/src/Root.js
@@ -1,51 +1,62 @@
 import React from 'react'
-import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BondeSessionProvider } from 'bonde-core-tools'
+/* import { ToastContainer } from 'react-toastify' */
 import { ProviderRedux } from './services/redux'
-import { ProviderGraphQL } from './services/graphql'
-import { ProviderLastLocation } from './services/router'
-import { SessionProvider, PrivateRoute, PublicRoute, Route } from './services/auth'
+/* import { ProviderGraphQL } from './services/graphql' */
+// import { ProviderLastLocation } from './services/router'
+// import { SessionProvider, PrivateRoute, PublicRoute, Route } from './services/auth'
 // Routes
 import Dashboard from './scenes/Dashboard'
-import { Root as AuthRoot } from './scenes/Auth'
-import { NotFound } from './components'
+// import { Root as AuthRoot } from './scenes/Auth'
+// import { NotFound } from './components'
 // Styles
-import 'react-toastify/dist/ReactToastify.css'
+// import 'react-toastify/dist/ReactToastify.css'
+
+const config = {
+  loginUrl: '/auth/login',
+  crossStorageUrl: process.env.REACT_APP_DOMAIN_CROSS_STORAGE || 'http://cross-storage.bonde.devel',
+  graphqlApiUrl: process.env.REACT_APP_DOMAIN_API_GRAPHQL || 'http://api-v2.bonde.devel/graphql'
+}
 
 const Root = () => (
-  <SessionProvider>
-    <ProviderGraphQL>
-      <ProviderRedux>
-        <React.Fragment>
-          <ToastContainer
-            className='BondeToastify'
-            hideProgressBar={true}
-          />
-          <Router>
-            <ProviderLastLocation>
-              <Switch>
-                <PublicRoute
-                  path='/auth'
-                  redirectTo='/admin'
-                  component={AuthRoot}
-                />
+  <BondeSessionProvider config={config}>
+    <ProviderRedux>
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <Dashboard />
+          </Route>
+        </Switch>
+{/*         <React.Fragment>
+        <ToastContainer
+          className='BondeToastify'
+          hideProgressBar={true}
+        />
+        <Router>
+          <ProviderLastLocation>
+            <Switch>
+              <PublicRoute
+                path='/auth'
+                redirectTo='/admin'
+                component={AuthRoot}
+              />
 
-                <PrivateRoute
-                  path='/admin'
-                  redirectTo='/auth/login'
-                  component={Dashboard}
-                />
+              <PrivateRoute
+                path='/admin'
+                redirectTo='/auth/login'
+                component={Dashboard}
+              />
 
-                <Redirect exact from='/' to='/admin' />
+              <Redirect exact from='/' to='/admin' />
 
-                <Route component={NotFound} />
-              </Switch>
-            </ProviderLastLocation>
-          </Router>
-        </React.Fragment>
-      </ProviderRedux>
-    </ProviderGraphQL>
-  </SessionProvider>
+              <Route component={NotFound} />
+            </Switch>
+          </ProviderLastLocation>
+        </React.Fragment> */}
+      </Router>
+    </ProviderRedux>
+  </BondeSessionProvider>
 )
 
 export default Root
--- packages/bonde-admin-canary/src/scenes/Dashboard/index.js
+++ packages/bonde-admin-canary/src/scenes/Dashboard/index.js
@@ -1,72 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { FullPageLoading } from 'bonde-styleguide'
+/* import { FullPageLoading } from 'bonde-styleguide' */
+import { BondeSessionUI } from 'bonde-core-tools'
 // local services & apps
-import { AuthProvider } from 'services/auth'
-import { PageLayout, TutorialPageLayout } from 'services/router'
-import { FullPageLayout, ContentPage } from './components'
+/* import { AuthProvider } from 'services/auth' */
+/* import { PageLayout, TutorialPageLayout } from 'services/router' */
+/* import { FullPageLayout, ContentPage } from './components' */
 // local scenes
-import ChatbotPage from './scenes/Chatbot'
-import HomePage from './scenes/Home'
-import SettingsPage from './scenes/Settings'
-import TagsPage from './scenes/Tags'
-import InvitationsPage from './scenes/Invitations'
+/* import ChatbotPage from './scenes/Chatbot' */
+/* import HomePage from './scenes/Home' */
+/* import SettingsPage from './scenes/Settings' */
+/* import TagsPage from './scenes/Tags' */
+/* import InvitationsPage from './scenes/Invitations' */
 
-const defaultProps = (title, header) => ({
+/* const defaultProps = (title, header) => ({
   title,
   wrapperHeaderComponent: header
-})
+}) */
+
+const Scene = () => (
+  <h3>Dashboard</h3>
+)
 
 const Dashboard = ({ match }) => {
   return (
-    <AuthProvider loading={FullPageLoading}>
-      <TutorialPageLayout
-        exact
-        path={match.path}
-        component={ContentPage}
-        pageProps={defaultProps('Inicio', HomePage.Header)}
-        componentProps={{ render: HomePage }}
-      />
-      <PageLayout path={`${match.path}/tags`} component={TagsPage} />
-      {/* Community Context */}
-      <FullPageLayout
-        path={`${match.path}/:communityId/invitations`}
-        component={ContentPage}
-        loading={FullPageLoading}
-        pageProps={defaultProps('Mobilizadores convidados')}
-        componentProps={{
-          title: 'Voltar',
-          backward: '/admin',
-          render: InvitationsPage
-        }}
-      />
-      {/* Configurações de Chatbot */}
-      <FullPageLayout
-        path={`${match.path}/:communityId/chatbot/:chatbotId`}
-        component={ChatbotPage}
-        loading={FullPageLoading}
-        pageProps={defaultProps('Chatbot')}
-        tabs={[
-          { name: 'Fluxos de conversas' },
-          { name: 'Menu Persistente', to: '/persistent-menu' },
-          { name: 'Configurações', to: '/settings' }
-        ]}
-      />
-      {/* Configurações de Comunidade */}
-      <FullPageLayout
-        path={`${match.path}/:communityId/settings`}
-        component={SettingsPage}
-        loading={FullPageLoading}
-        pageProps={defaultProps('Configurações')}
-        tabs={[
-          { name: 'Informações' },
-          { name: 'Mobilizadores', to: '/invite' },
-          { name: 'Domínios', to: '/domain' },
-          { name: 'Integrações', to: '/integration' },
-          { name: 'Financeira', to: '/recipient' }
-        ]}
-      />
-    </AuthProvider>
+    <BondeSessionUI.Main indexRoute='/'>
+      <BondeSessionUI.Content>
+        <Scene />
+      </BondeSessionUI.Content>
+    </BondeSessionUI.Main>
   )
 }
 
@@ -77,3 +39,50 @@ Dashboard.propTypes = {
 }
 
 export default Dashboard
+
+// {/* <TutorialPageLayout
+//         exact
+//         path={match.path}
+//         component={ContentPage}
+//         pageProps={defaultProps('Inicio', HomePage.Header)}
+//         componentProps={{ render: HomePage }}
+//       />
+//       <PageLayout path={`${match.path}/tags`} component={TagsPage} /> */}
+//       {/* Community Context */}
+//       {/* <FullPageLayout
+//         path={`${match.path}/:communityId/invitations`}
+//         component={ContentPage}
+//         loading={FullPageLoading}
+//         pageProps={defaultProps('Mobilizadores convidados')}
+//         componentProps={{
+//           title: 'Voltar',
+//           backward: '/admin',
+//           render: InvitationsPage
+//         }}
+//       /> */}
+//       {/* Configurações de Chatbot */}
+//       {/* <FullPageLayout
+//         path={`${match.path}/:communityId/chatbot/:chatbotId`}
+//         component={ChatbotPage}
+//         loading={FullPageLoading}
+//         pageProps={defaultProps('Chatbot')}
+//         tabs={[
+//           { name: 'Fluxos de conversas' },
+//           { name: 'Menu Persistente', to: '/persistent-menu' },
+//           { name: 'Configurações', to: '/settings' }
+//         ]}
+//       /> */}
+//       {/* Configurações de Comunidade */}
+//       {/* <FullPageLayout
+//         path={`${match.path}/:communityId/settings`}
+//         component={SettingsPage}
+//         loading={FullPageLoading}
+//         pageProps={defaultProps('Configurações')}
+//         tabs={[
+//           { name: 'Informações' },
+//           { name: 'Mobilizadores', to: '/invite' },
+//           { name: 'Domínios', to: '/domain' },
+//           { name: 'Integrações', to: '/integration' },
+//           { name: 'Financeira', to: '/recipient' }
+//         ]}
+//       /> */}
