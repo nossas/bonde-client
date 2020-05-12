e9b5f8fc1fd7e5e0ab7cf5c3040eaba0c6cb6c0e
Igor Santos <igr.exe@gmail.com>
2020-05-12 10:31:43 -0300

    fix(public): add parse to fields on form widget

--- packages/bonde-public/src/plugin-form.connected.js
+++ packages/bonde-public/src/plugin-form.connected.js
@@ -6,6 +6,19 @@ import { selectors as MobSelectors } from 'bonde-webpage/lib/redux'
 
 const mapDispatchToProps = { asyncFormEntryCreate }
 
-const mapStateToProps = state => MobSelectors(state).getMobilizationLink()
+const mapStateToProps = (state, { widget, ...props }) => {
+  const mobilizationLink = MobSelectors(state).getMobilizationLink()
+  return {
+    ...props,
+    ...mobilizationLink,
+    widget: {
+      ...widget,
+      settings: {
+        ...widget.settings,
+        fields: JSON.parse(widget.settings.fields)
+      }
+    }
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(FormPlugin))
